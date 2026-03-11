fa58512f836d8cda87820f975793dbba83119467
Mzenao <mzenao@hotmail.com>
2026-03-10 23:08:33 -0300

    Ajustes na configuração do vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
   preview: {
     host: true,
-    allowedHosts: ["odontologia-ello.up.railway.app"]
+    allowedHosts: true
   }
 })
